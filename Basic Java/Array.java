f411bf50ba6ab76305161f738211bf064f1d8a0d
vishalkr90 <vishal98kumarr@gmail.com>
2026-07-24 23:11:02 +0530

    Updated Array.java

--- Basic Java/Array.java
+++ Basic Java/Array.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 public class Array {
     //Array traversal
         public static void main(String[] args) {
-        int[] myArr = {98, 2, 8, 65, 45};
-        for(int index = 0; index <= myArr.length;index ++){
-        System.out.println(myArr[index]);
+    //     int[] myArr = {98, 2, 8, 65, 45};
+    //     for(int index = 0; index <= myArr.length;index ++){
+    //     System.out.println(myArr[index]);
+    //     }
+    // }
+    int arr[] = {3,5,2,3};
+    int val = 3;
+    int count = 0;
+    for(int i = 0;i<arr.length;i++){
+        if(val != arr[i]){
+            arr[count] = arr[i];
+            count++;
         }
     }
+    System.out.println("Count = "+count);
+    for(int i = 0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+        }
 }
