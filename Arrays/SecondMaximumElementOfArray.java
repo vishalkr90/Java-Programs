dcd9f9dcf47dda0fb788a1b51e58ce6def4b4d01
vishalkr90 <vishal98kumarr@gmail.com>
2026-07-26 22:17:51 +0530

    .

--- Arrays/SecondMaximumElementOfArray.java
+++ Arrays/SecondMaximumElementOfArray.java
@@ -6,6 +6,7 @@ public class SecondMaximumElementOfArray {
         for(int i = 0;i<arr.length;i++){
             if(arr[i]>max){
                 max = arr[i];
+                
             }
         }
         System.out.println("The maximum number is : "+max);
