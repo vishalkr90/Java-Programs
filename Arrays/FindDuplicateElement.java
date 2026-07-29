bbd60855ed2f30a5b38b585c7a9c5bba5ab8ef6f
vishalkr90 <vishal98kumarr@gmail.com>
2026-07-29 23:52:54 +0530

    h

--- Arrays/FindDuplicateElement.java
+++ Arrays/FindDuplicateElement.java
@@ -1,18 +1,34 @@
 public class FindDuplicateElement {
-    public static void main(String[] args) {
-    int[] arr = {2,3,1,4,2};
-    int n = arr.length;
-    boolean[] flag = new boolean[n + 1];
-
-    for (int i = 0; i < n; i++) {
-    int ele = arr[i];
-
-    if (flag[ele]) {
-        System.out.println("Duplicate = " + ele);
-    } 
-    else {
-        flag[ele] = true;
+    public static int dup(int arr[]){
+        int n = arr.length;
+        int num = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if(arr[i] == arr[j]){
+                    num = arr[i];
+                }
+            }
+        }
+        return num;
     }
-}
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,2,2};
+        int result = dup(arr);
+        System.out.println(result);
+        
+//     int[] arr = {2,3,1,4,2};
+//     int n = arr.length;
+//     boolean[] flag = new boolean[n + 1];
+
+//     for (int i = 0; i < n; i++) {
+//     int ele = arr[i];
+
+//     if (flag[ele]) {
+//         System.out.println("Duplicate = " + ele);
+//     } 
+//     else {
+//         flag[ele] = true;
+//     }
+// }
     }
 }
--- PatternPrinting/AlphaNumericTriangle.java
+++ PatternPrinting/AlphaNumericTriangle.java
@@ -6,16 +6,17 @@ public class AlphaNumericTriangle {
         System.out.print("Enter Row : ");
         int n = sc.nextInt();
         
-        for(int i=1;i<=n;i++){// Lines
-            for(int j = 1;j<=i;j++){ // How much star print in line
-             if(i%2==0){
-                System.out.print((char)(j+64)+" ");
-             }  
-            else{
-                System.out.print(j+" ");
-            }
-            }
-            System.out.println();
-        }
+        // for(int i=1;i<=n;i++){// Lines
+        //     for(int j = 1;j<=i;j++){ // How much star print in line
+        //      if(i%2==0){
+        //         System.out.print((char)(j+64)+" ");
+        //      }  
+        //     else{
+        //         System.out.print(j+" ");
+        //     }
+        //     }
+        //     System.out.println();
+        // }
+        
     }
 }
