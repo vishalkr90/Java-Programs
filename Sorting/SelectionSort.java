e586905863586a6612b14089911c8c36df5a7a0e
vishalkr90 <vishal98kumarr@gmail.com>
2026-08-02 21:02:53 +0530

    Selection

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,14 +1,27 @@
 public class SelectionSort {
     public static void select(int arr[]){
         int n = arr.length;
-        for(int i = 0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
-                if(arr[i]>arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        // for(int i = 0;i<n-1;i++){
+        //     for(int j = i+1;j<n;j++){
+        //         if(arr[i]>arr[j]){
+        //             int temp = arr[i];
+        //             arr[i] = arr[j];
+        //             arr[j] = temp;
+        //         }
+        //     }
+        // }
+        for(int i = 0;i<n;i++){
+            int min = Integer.MAX_VALUE;
+            int index = 1;
+            for(int j = i;j<n;j++){
+                if(arr[j]<min){
+                    min = arr[j];
+                    index = j;
                 }
             }
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
         }
         System.out.print("The array is after Sorting ; ");
         for(int i = 0;i<n;i++){
