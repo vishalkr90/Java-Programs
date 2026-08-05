25948b5d24925311de95096279459b37843c9f22
vishalkr90 <vishal98kumarr@gmail.com>
2026-08-06 00:09:03 +0530

    BinarySearch

---
+++ Binary Search/BinarySearchh.java
@@ -0,0 +1,26 @@
+public class BinarySearchh {
+    static int search(int arr[],int target){
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+        int mid;
+        while(low<=high){
+            mid = (low + high)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                low = mid + 1;
+            }
+            else if(arr[mid]>target){
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] ={3,4,6,7,8};
+        int result = search(arr,9);
+        System.out.println(result);
+    }
+}
